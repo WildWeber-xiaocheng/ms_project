7952058c191141b42acd2877f9be791a467a08a9
100ask <100ask@100ask.com>
2024-12-29 23:10:05 +0800

    add:添加 project-project模块以及/project/index接口的实现

---
+++ project-project/config/config.go
@@ -0,0 +1,145 @@
+package config
+
+import (
+	"github.com/redis/go-redis/v9"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"test.com/project-common/logs"
+)
+
+var Conf = InitConfig()
+
+type Config struct {
+	viper       *viper.Viper
+	SC          *ServerConfig
+	GC          *GrpcConfig
+	EtcdConfig  *EtcdConfig
+	MysqlConfig *MysqlConfig
+	JwtConfig   *JwtConfig
+}
+
+type ServerConfig struct {
+	Name string
+	Addr string
+}
+
+type GrpcConfig struct {
+	Name    string
+	Addr    string
+	Version string
+	Weight  int64
+}
+
+type EtcdConfig struct {
+	Addrs []string
+}
+
+type MysqlConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	Db       string
+}
+
+type JwtConfig struct {
+	AccessExp     int
+	RefreshExp    int
+	AccessSecret  string
+	RefreshSecret string
+}
+
+func InitConfig() *Config {
+	v := viper.New()
+	conf := &Config{viper: v}
+	workDir, _ := os.Getwd()
+	conf.viper.SetConfigName("config")
+	conf.viper.SetConfigType("yaml")
+	conf.viper.AddConfigPath(workDir + "/config")
+
+	err := conf.viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln(err)
+		return nil
+	}
+	conf.ReadServerConfig()
+	conf.InitZapLog()
+	conf.ReadGrpcConfig()
+	conf.ReadEtcdConfig()
+	conf.InitMysqlConfig()
+	conf.InitJwtConfig()
+	return conf
+}
+
+func (c *Config) ReadServerConfig() {
+	sc := &ServerConfig{}
+	sc.Name = c.viper.GetString("server.name")
+	sc.Addr = c.viper.GetString("server.addr")
+	c.SC = sc
+}
+
+func (c *Config) ReadGrpcConfig() {
+	gc := &GrpcConfig{}
+	gc.Name = c.viper.GetString("grpc.name")
+	gc.Addr = c.viper.GetString("grpc.addr")
+	gc.Version = c.viper.GetString("grpc.version")
+	gc.Weight = c.viper.GetInt64("grpc.weight")
+	c.GC = gc
+}
+
+func (c *Config) ReadEtcdConfig() {
+	ec := &EtcdConfig{}
+	var addrs []string
+	err := c.viper.UnmarshalKey("etcd.addrs", &addrs)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	ec.Addrs = addrs
+	c.EtcdConfig = ec
+}
+
+func (c *Config) InitZapLog() {
+	//从配置中读取日志配置，初始化日志
+	lc := &logs.LogConfig{
+		DebugFileName: "C:\\goCode\\ms_project\\logs\\debug\\project-debug.log",
+		InfoFileName:  "C:\\goCode\\ms_project\\logs\\info\\project-info.log",
+		WarnFileName:  "C:\\goCode\\ms_project\\logs\\error\\project-error.log",
+		MaxSize:       500,
+		MaxAge:        28,
+		MaxBackups:    3,
+	}
+	err := logs.InitLogger(lc)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (c *Config) InitRedisOptions() *redis.Options {
+	return &redis.Options{
+		Addr:     c.viper.GetString("redis.host") + ":" + c.viper.GetString("redis.port"),
+		Password: c.viper.GetString("redis.password"),
+		DB:       c.viper.GetInt("redis.db"), // use default DB
+	}
+}
+
+func (c *Config) InitMysqlConfig() {
+	mc := &MysqlConfig{
+		Username: c.viper.GetString("mysql.username"),
+		Password: c.viper.GetString("mysql.password"),
+		Host:     c.viper.GetString("mysql.host"),
+		Port:     c.viper.GetInt("mysql.port"),
+		Db:       c.viper.GetString("mysql.db"),
+	}
+	c.MysqlConfig = mc
+}
+
+func (c *Config) InitJwtConfig() {
+	mc := &JwtConfig{
+		AccessExp:     c.viper.GetInt("jwt.accessExp"),
+		RefreshExp:    c.viper.GetInt("jwt.refreshExp"),
+		AccessSecret:  c.viper.GetString("jwt.accessSecret"),
+		RefreshSecret: c.viper.GetString("jwt.RefreshSecret"),
+	}
+	c.JwtConfig = mc
+}
