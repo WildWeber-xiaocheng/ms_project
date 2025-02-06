54b6d8940c724bd66a5c29c1e53ed143c19b28b6
100ask <100ask@100ask.com>
2025-02-06 23:20:20 +0800

    add: "访问节点"接口

---
+++ project-project/internal/domain/project_node.go
@@ -0,0 +1,30 @@
+package domain
+
+import (
+	"context"
+	"test.com/project-common/errs"
+	"test.com/project-project/internal/dao"
+	"test.com/project-project/internal/data"
+	"test.com/project-project/internal/repo"
+	"test.com/project-project/pkg/model"
+)
+
+type ProjectNodeDomain struct {
+	projectNodeRepo repo.ProjectNodeRepo
+}
+
+func (d *ProjectNodeDomain) TreeList() ([]*data.ProjectNodeTree, *errs.BError) {
+	//node表都查出来，转换成treelist结构
+	nodes, err := d.projectNodeRepo.FindAll(context.Background())
+	if err != nil {
+		return nil, model.DBError
+	}
+	treeList := data.ToNodeTreeList(nodes)
+	return treeList, nil
+}
+
+func NewProjectNodeDomain() *ProjectNodeDomain {
+	return &ProjectNodeDomain{
+		projectNodeRepo: dao.NewProjectNodeDao(),
+	}
+}
